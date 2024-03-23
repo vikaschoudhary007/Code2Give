3c46ecf46a91dcef7beb00cd79701163e162efcc
SCodrean <stefanvcodrean@gmail.com>
2024-03-23 14:28:06 -0400

    Created Navbar

---
+++ front_end/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from "./Header"
+import EmployeeForm from "./EmployeeForm"
+import Contact from "./Contact"
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      {/* <Header /> */}
+      {/* <Contact /> */}
+    </div>
+  );
+}
+
+export default App;
